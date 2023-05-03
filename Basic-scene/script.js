bb741e5d662250cc195454ca38a9357ad2170ee7
Shubham Kumar Singh <denyshubham@gmail.com>
2023-05-03 20:46:41 +0000

    updated basic-scene

--- Basic-scene/script.js
+++ Basic-scene/script.js
@@ -1,3 +1,6 @@
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
 // Scene
 const scene = new THREE.Scene()
 
@@ -20,4 +23,13 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
 scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
